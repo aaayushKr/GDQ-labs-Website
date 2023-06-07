2cf5573845f0d006ee6ce5c909a0f02b4860c6b9
aaayushKr <100994359+aaayushKr@users.noreply.github.com>
2023-06-07 19:04:03 +0530

    Add files via upload

--- slider.js
+++ slider.js
@@ -42,3 +42,54 @@ sliders.forEach((slider) => {
 });
 
 //slider
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollContainers = document.querySelectorAll(".scroll");
+  const leftButtons = document.querySelectorAll(".left-btn");
+  const rightButtons = document.querySelectorAll(".right-btn");
+
+  scrollContainers.forEach((scrollContainer, index) => {
+    let isDown = false;
+    let scrollX;
+    let scrollLeft;
+
+    // Mouse Up Function
+    scrollContainer.addEventListener("mouseup", () => {
+      isDown = false;
+      scrollContainer.classList.remove("active");
+    });
+
+    // Mouse Leave Function
+    scrollContainer.addEventListener("mouseleave", () => {
+      isDown = false;
+      scrollContainer.classList.remove("active");
+    });
+
+    // Mouse Down Function
+    scrollContainer.addEventListener("mousedown", (e) => {
+      e.preventDefault();
+      isDown = true;
+      scrollContainer.classList.add("active");
+      scrollX = e.pageX - scrollContainer.offsetLeft;
+      scrollLeft = scrollContainer.scrollLeft;
+    });
+
+    // Mouse Move Function
+    scrollContainer.addEventListener("mousemove", (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const element = e.pageX - scrollContainer.offsetLeft;
+      const scrolling = (element - scrollX) * 2;
+      scrollContainer.scrollLeft = scrollLeft - scrolling;
+    });
+
+    // Left Button Click Function
+    leftButtons[index].addEventListener("click", () => {
+      scrollContainers[index].scrollLeft -= 500; // Adjust the scrolling amount as needed
+    });
+
+    // Right Button Click Function
+    rightButtons[index].addEventListener("click", () => {
+      scrollContainers[index].scrollLeft += 500; // Adjust the scrolling amount as needed
+    });
+  });
+});
